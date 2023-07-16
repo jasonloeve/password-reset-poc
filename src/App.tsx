1ee02184a61ffd0e0c9d7db573e88687f5fde389
Jason Loeve <jasonloeve@Jasons-MacBook-Pro.local>
2023-07-16 20:26:01 +1200

    Scaffolding base react application

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent } from 'react';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const handlePasswordChange = ({ target }: ChangeEvent<HTMLInputElement>): void => {
+    const { value } = target;
+
+    // Debug
+    console.log(value);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+
+    // Debug
+    console.log('Catch Submit')
+  }
+
   return (
     <div className="App">
-      <div>
-        <div>Heading</div>
-        <div>Form</div>
+      <div className="Container">
+        <h1 className="Heading">Reset your password</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="password" onChange={handlePasswordChange}/>
+          <button type="submit">Click me</button>
+        </form>
       </div>
     </div>
   );
